bcb70f9da62fe26ac6c984b28d08302cfee057dc
ubuntu030 <ubuntu030@gmail.com>
2020-12-08 10:14:17 +0800

    Album 元件 unsplash 元件

---
+++ src/components/unsplash.js
@@ -0,0 +1,7 @@
+import { createApi } from 'unsplash-js';
+
+const unsplash = createApi({
+	accessKey: '39181425fbdbae9517bc80c58841ac99e98425d8581a7976e388114f8e47f6db',
+});
+
+export default unsplash;
